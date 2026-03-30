547969c958bb3678f0fa76d51645709347320935
xybayue-tbk <xybayue@gmail.com>
2026-03-30 16:36:38 +0700

    Perulangan dengan WHILE

--- looping.cpp
+++ looping.cpp
@@ -8,11 +8,15 @@ int main(){
     //Perulangan dengan FOR
     //mencetak tulisan sebanyak 5 kali
     cout<<"PERULANGAN FOR"<<endl;
-for(int i=0;i<=4;i++){
+    for(int i=0;i<=4;i++){
     cout<<"Teknologi Informasi UMY"<<endl;
 }
     cout<<endl;
 
     //untuk mendapatkan urutan angka acak yg berbeda
     srand(time(0));
+
+     //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
 }
