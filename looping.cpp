040affb527a30895837521c5227b9c15d21346b5
xybayue-tbk <xybayue@gmail.com>
2026-03-30 16:31:57 +0700

    Perulangan FOR

--- looping.cpp
+++ looping.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main(){
     int x;
+    
+    //Perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+for(int i=0;i<=4;i++){
+    cout<<"Teknologi Informasi UMY"<<endl;
+}
 }
