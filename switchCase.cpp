2fb15daa09842d946cd1f5adeb12c903e7f6c3d7
xybayue-tbk <xybayue@gmail.com>
2026-03-30 17:25:26 +0700

    prosedur output

--- switchCase.cpp
+++ switchCase.cpp
@@ -34,3 +34,10 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
