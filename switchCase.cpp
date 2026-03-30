a43400ae75d2ffe51b8f24453ca5dc82d5f9e617
xybayue-tbk <xybayue@gmail.com>
2026-03-30 17:20:21 +0700

    fungsi input

--- switchCase.cpp
+++ switchCase.cpp
@@ -4,3 +4,13 @@ using namespace std;
 int kodeJurusan;
 string namaJurusan;
 string statusKuota;
+
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
