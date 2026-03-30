2d02c483ede36ddd678c8d232930992ef2d44d81
xybayue-tbk <xybayue@gmail.com>
2026-03-30 16:49:24 +0700

    Loop DO WHILE

--- looping.cpp
+++ looping.cpp
@@ -31,4 +31,8 @@ int main(){
     //Perulangan dengan DO...WHILE
     cout<<"PERULANGAN DO...WHILE"<<endl;
     x = 1 + rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }while(x<=5);
 }
